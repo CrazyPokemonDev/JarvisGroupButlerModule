888c9133c64e33e5dc1687d029b7c6aaad354040
Florian Meyer <florian.meyer@capgemini.com>
2019-11-11 16:55:31 +0100

    Add project files.

---
+++ JarvisGroupButlerModule/GroupButlerModule.cs
@@ -0,0 +1,24 @@
+﻿using JarvisModuleCore.Attributes;
+using JarvisModuleCore.Classes;
+using JarvisModuleCore.ML;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JarvisGroupButlerModule
+{
+    [JarvisModule]
+    public class GroupButlerModule : JarvisModule
+    {
+        public override string Id => "jarvis.official.groupbutler";
+        public override string Name => "Group butler";
+        public override Version Version => Version.Parse("1.0.0");
+        private const string mlDataFilePath = "Training\\data.json";
+        public override TaskPredictionInput[] MLTrainingData => JsonConvert.DeserializeObject<TaskPredictionInput[]>(File.ReadAllText(mlDataFilePath));
+    }
+}
