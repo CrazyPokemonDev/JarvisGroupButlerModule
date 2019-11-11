7185497f7ffe13ae2b78a7a977e1774129de3b1c
Florian Meyer <florian.meyer@capgemini.com>
2019-11-11 21:24:35 +0100

    create base dir if not exists on startup

--- JarvisGroupButlerModule/GroupButlerModule.cs
+++ JarvisGroupButlerModule/GroupButlerModule.cs
@@ -36,6 +36,7 @@ namespace JarvisGroupButlerModule
         #region Startup
         public GroupButlerModule()
         {
+            Directory.CreateDirectory(baseDirectory);
             db = new JarvisContext(dbFilePath);
         }
 
